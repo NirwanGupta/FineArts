4594cd06a25ff89470d2adf44a35a979359675e7
NirwanGupta <guptanirwan@gmail.com>
2024-07-27 19:57:33 +0530

    allies correction

--- client/src/Components/Allies/Allies.jsx
+++ client/src/Components/Allies/Allies.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SingleAllie from './SingleAllie';
-import './Allies.css';
+import './newAllies.css'; // Adjusted CSS file import
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import Navbar from '../NavBar/Navbar';
 import alliesArt from '../../images/art-image-allies.svg';
@@ -10,97 +10,82 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Allies = () => {
     const [list, setList] = useState([]);
-    const [currentPerson, setCurrentPerson] = useState(0);
     const [filteredAllies, setFilteredAllies] = useState([]);
     const [value, setValue] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         const fetchList = async () => {
             try {
                 const response = await axios.get(`api/allAllies`);
                 const data = response.data;
-                // Sort data in decreasing order based on year
-                const sortedData = data.sort((a, b) => Number(b.year) - Number(a.year));
-                setList(sortedData);
-                setFilteredAllies(sortedData);
+                setList(data);
+                const filtered = data.filter((allie) => Number(allie.year) === new Date().getFullYear());
+                setFilteredAllies(filtered);
+                setIsLoading(false);
             } catch (error) {
                 toast.error("There was an error fetching the allies");
+                setIsLoading(false);
+                setIsError(true);
                 console.log(error);
-            }
+            } 
         }
         fetchList();
     }, []);
-
-    const prevSlide = () => {
-        setCurrentPerson((oldPerson) => {
-            return (oldPerson - 1 + filteredAllies.length) % filteredAllies.length;
-        })
-    };
-    const nextSlide = () => {
-        setCurrentPerson((oldPerson) => {
-            return (oldPerson + 1) % filteredAllies.length;
-        })
-    };
 
     const filterAllies = (e) => {
         e.preventDefault();
         let year = value;
         if (!year || isNaN(Number(year))) {
-            year = new Date().getFullYear();
-            setFilteredAllies(list);
+            const filtered = list.filter((allie) => Number(allie.year) === new Date().getFullYear());
+            setFilteredAllies(filtered);
             toast.error('Please enter a valid year');
             return;
         }
         if (Number(year) < 2021) {
-            year = new Date().getFullYear();
-            setFilteredAllies(list);
+            const filtered = list.filter((allie) => Number(allie.year) === new Date().getFullYear());
+            setFilteredAllies(filtered);
             toast.error('Please enter year greater than 2021');
             return;
         }
         if (Number(year) > new Date().getFullYear()) {
-            year = new Date().getFullYear();
-            setFilteredAllies(list);
+            const filtered = list.filter((allie) => Number(allie.year) === new Date().getFullYear());
+            setFilteredAllies(filtered);
             toast.error('Please enter year less than current year');
             return;
         }
 
-        // console.log(year, typeof (year));
         const filtered = list.filter(allie => Number(allie.year) === Number(year));
-        // console.log(filtered);
         toast.success('Allies filtered successfully');
         setFilteredAllies(filtered);
     };
 
+    if(isLoading) {
+        return <div className="loading"></div>
+    }
+    if(isError) {
+        return <div className="error">There was an error...</div>
+    }
+
     return (
         <div className="allies-super-container">
             <Navbar />
-            <div className="allies-title">
-                <h1>Our Allies</h1>
-            </div>
-            <div className="allies-flex-container">
-                <div className='allies-left'>
-                    <div className="allies-form">
-                        <form onSubmit={filterAllies} className='form'>
-                            <label htmlFor="year">
-                                <input type='text' id="year" onChange={(e) => setValue(e.target.value)} className='allie-year-input' />
-                            </label>
-                            <button type="submit" className='allie-submit-btn'>Filter</button>
-                        </form>
+            <div className="allies-container">
+                <div className="allie-heading">
+                    <h1 className='heading-allies'>Our Allies</h1>
+                    <div className="filter-allies">
+                        <input type='text' className="allies-input" placeholder='Filter Allies' onChange={(e) => setValue(e.target.value)} />
+                        <button type='submit' className='allies-submit-btn' onClick={filterAllies}>Filter</button>
                     </div>
-                    <section className="allies-container">
-                        {filteredAllies.map((item, index) => {
-                            return <SingleAllie key={item.id} {...item} currentPerson={currentPerson} personIndex={index} />
-                        })}
-                        <button type="button" className="allies-prev" onClick={prevSlide}>
-                            <FiChevronLeft className='allies-icon' />
-                        </button>
-                        <button type="button" className="allies-next" onClick={nextSlide}>
-                            <FiChevronRight className='allies-icon' />
-                        </button>
-                    </section>
                 </div>
-                <img src={alliesArt} alt="allies-logo" className='allies-logo' />
+                <ul id="hexGrid">
+                    {filteredAllies.map((allie, index) => (
+                        <SingleAllie key={index} {...allie} />
+                    ))}
+                </ul>
             </div>
+            <a id="fork" target="_blank" href="https://github.com/web-tiki/responsive-grid-of-hexagons">Fork on GitHub</a>
             <ToastContainer />
         </div>
     );
--- client/src/Components/Allies/SingleAllie.jsx
+++ client/src/Components/Allies/SingleAllie.jsx
@@ -3,36 +3,28 @@ import './SingleAllie.css';
 import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const SingleAllie = ({id, name, image, year, text, currentPerson, personIndex}) => {
-    // console.log(id, name, year);
+    console.log(id, name, year, image);
     return (
-        <div className="single-allie" style={{transform: `translateX(${100*(personIndex-currentPerson)}%`, opacity: personIndex === currentPerson? 1: 0, visibility: personIndex === currentPerson? 'visible': 'hidden'}}>
-            <div className="allie-image">
+        <li class="hex">
+            <a class="hexIn">
                 <img src={image} alt={name} />
-            </div>
-            <div className="allie-name">
-                <h4>{name}</h4>
-            </div>
-            <div className="allie-year">
-                <h5>{year} - {(Number(year)+1)}</h5>
-            </div>
-            <div className="allie-text">
-                <p>{text}</p>
-            </div>
-            <div className="allie-social-icons">
-                <a href="https://www.facebook.com">
-                    <FaFacebook />
-                </a>
-                <a href="https://www.instagram.com">
-                    <FaInstagram />
-                </a>
-                <a href="https://x.com/">
-                    <FaTwitter />
-                </a>
-                <a href="https://www.linkedin.com">
-                    <FaLinkedin />
-                </a>
-            </div>
-        </div>
+                <h1>{name}</h1>
+                <p className="single-allie-icon">
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" style={{left: '15%'}}>
+                        <FaInstagram />
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" style={{left: '35%'}}>
+                        <FaFacebook />
+                    </a>
+                    <a href="https://www.x.com" target="_blank" rel="noopener noreferrer" style={{left: '55%'}}>
+                        <FaTwitter />
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" style={{left: '75%'}}>
+                        <FaLinkedin />
+                    </a>
+                </p>
+            </a>
+        </li>
     );
 };
 
